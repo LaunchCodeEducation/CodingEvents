e6bf0278e03850986915a4bb9065c9524b6fb90a
Sally Steuterman <sally.steuterman@gmail.com>
2023-02-16 09:11:09 -0600

    views-setup

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<string> Events = new List<string>();
+            Events.Add("Code With Pride");
+            Events.Add("Apple WWDC");
+            Events.Add("Strange Loop");
+
+            ViewBag.events = Events;
+
+            return View();
+        }
+    }
+}
+
