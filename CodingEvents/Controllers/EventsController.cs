8c84cc4f054fab7d6f138ddcd092d12167b75977
Sally Steuterman <sally.steuterman@gmail.com>
2023-02-27 10:12:39 -0600

    Walkthrough of models chapter

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,12 +12,10 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
-
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
 
             return View();
         }
@@ -28,9 +28,27 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name, string desc = "")
+        public IActionResult NewEvent(Event newEvent)
         {
-            Events.Add(name, desc);
+            EventData.Add(newEvent);
+
+            return Redirect("/Events");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
 
             return Redirect("/Events");
         }
---
+++ CodingEvents/Models/Event.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace CodingEvents.Models
+{
+	public class Event
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		public int Id { get; set; }
+		static private int nextId = 1;
+
+		public Event()
+		{
+			Id = nextId;
+			nextId++;
+		}
+
+		public Event(string name, string description)
+		{
+			Name = name;
+			Description = description;
+			Id = nextId;
+			nextId++;
+		}
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+			return obj is Event @event && Id == @event.Id;
+        }
+
+        public override int GetHashCode()
+        {
+			return HashCode.Combine(Id);
+        }
+    }
+}
+
