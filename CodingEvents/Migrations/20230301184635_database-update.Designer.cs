160e66b3a6ae181d00baaa0c99508e5630539ca0
John Woolbright <jwoolbright23@gmail.com>
2023-03-01 12:56:51 -0600

    final migrations and minor changes to _layout.cshtml

--- CodingEvents/Migrations/20230301184635_database-update.Designer.cs
+++ CodingEvents/Migrations/20230301184635_database-update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CodingEvents.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CodingEvents.Migrations
 {
     [DbContext(typeof(EventDbContext))]
-    partial class EventDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230301184635_database-update")]
+    partial class databaseupdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
