58c7d26c648a0010ef79f0fb4ea44ea0663e2f40
John Woolbright <jwoolbright23@gmail.com>
2023-03-09 15:06:31 -0600

    walkthrough steps for some-setup chapter

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -79,6 +79,16 @@ namespace CodingEvents.Controllers
 
             return Redirect("/Events");
         }
+
+        public IActionResult Detail(int id)
+        {
+        Event theEvent = context.Events
+            .Include(e => e.Category)
+            .Single(e => e.Id == id);
+
+        EventDetailViewModel viewModel = new EventDetailViewModel(theEvent);
+        return View(viewModel);
+        }
     }
 }
 
